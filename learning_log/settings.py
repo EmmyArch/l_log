e7a483f0c5906dd3cf43f76b097a29a1b5159b8f
ll_admin <ll_admin@gmail.com>
2024-12-09 13:42:16 +0100

    migrated to a postgres db

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -83,12 +83,29 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+
+# old database
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+
+
+# new database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'll_db',
+        'USER': 'postgres',
+        'PASSWORD': 'grey5050',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
 }
+
 
 
 # Password validation
