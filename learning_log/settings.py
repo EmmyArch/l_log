163be763e30c77ffbbefa2e1b6ffa3ba2372cb3e
ll_admin <ll_admin@gmail.com>
2024-12-10 15:55:07 +0100

    deploy state 1

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q6d+n8@i*c9-*@93aeuc$(y=vyu1gf_d8%w2v*vr5+czdiq@%+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
