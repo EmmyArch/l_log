eff0d3b233f7273e4f5f3230a611192e66b3e59d
EmmyArch <heyyo.its.mzee@gmail.com>
2024-12-11 08:36:27 +0100

    Update settings.py
    added to local host

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-q6d+n8@i*c9-*@93aeuc$(y=vyu1gf_d8%w2v*vr5+czdiq@%+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ["127.0.0.1", '.vercel.app', ".now.sh"]
 
 
 # Application definition
