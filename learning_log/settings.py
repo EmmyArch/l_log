17d5050b776811becc60323bedc5632aa327bbff
ll_admin <ll_admin@gmail.com>
2024-12-11 10:44:56 +0100

    set debug to false

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q6d+n8@i*c9-*@93aeuc$(y=vyu1gf_d8%w2v*vr5+czdiq@%+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", '.vercel.app', ".now.sh"]
 
